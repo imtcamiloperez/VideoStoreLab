ff5176644c357f96a011c7763d9a80bbb7f2bce9
cabrerajm <jcabrera1@gmail.com>
2022-05-18 14:18:16 -0500

    principio de unico proposito y separacion de funciones pequeñas

--- src/Statement.java
+++ src/Statement.java
@@ -27,7 +27,10 @@ public class Statement {
     }
 
     public void rentalLines(Rental arg) {
-        rentals.add(arg);
+        String rentalLines ="";
+        for (Rental rental : rentals)
+            rentalLines += rentalLine(rental);
+        return rentalLines;
     }
 
     private String header() {
@@ -36,54 +39,43 @@ public class Statement {
 
     double thisAmount = 0;
 
-    // determine amount for each line
-            switch(rental.getMovie().
+    private rentalLine(Rental rental){
+        double rentalAmount = determineAmount(rental);
+        frequentRenterPoints = determineFrequentRenterPoints(rental);
+        totalAmount += rentalAmount;
 
-    getPriceCode())
+        return formatRentalLine(rental, rentalAmount);
 
-    {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (rental.getDaysRented() > 2)
-                thisAmount += (rental.getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += rental.getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (rental.getDaysRented() > 3)
-                thisAmount += (rental.getDaysRented() - 3) * 1.5;
-            break;
+
     }
 
-    // add frequent renter points
-    frequentRenterPoints ++;
-    // add bonus for a two day new release rental
-            if((rental.getMovie().
+    private String formatRentalLine(Rental rental, double rentalAmount){
+        return String.format("\t%s\t%.1f\n", rental.getMovie().getTitle(),rentalAmount);
+    }
 
-    getPriceCode() ==Movie.NEW_RELEASE)&&
-            rental.getDaysRented()>1)frequentRenterPoints ++;
-
-    //show figures for this rental
-    statementText +="\t"+rental.getMovie().
-
-    getTitle()+"\t"+
-            String.valueOf(thisAmount)+"\n";
-    totalAmount +=thisAmount;
-
-    //add footer lines
-    statementTxt +="You owed "+String.valueOf(totalAmount)+"\n";
-    statementTxt +="You earned "+String.valueOf(frequentRenterPoints)+
-            " frequent renter points\n";
-        return statementTxt;
-
-
-}
-
-
-
-
+    private int  determineFrequentRenterPoints(Rental rental){
+        boolean bonusIsEarned = rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
+        if (bonusIsEarned)
+            return 2;
+        return 1;
+    }
+    private double determineAmount(Rental rental) {
+        double rentalAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    rentalAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    rentalAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
 
     public String getCustomerName() {
         return customerName;
